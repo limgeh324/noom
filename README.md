12c4b4c25ab2d3152f59acb66f8e4e0fe562300f
LIMGEH <limgeh324@gmail.com>
2022-04-03 19:06:13 +0900

    start_project

---
+++ README.md
@@ -0,0 +1,3 @@
+# NOOM
+
+Zoom Clone using Nodejs, WebRTC and Websockets.
---
+++ src/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+const handleListen = () => {
+  console.log("Listening on port http://localhost:3000");
+};
+
+app.listen(3000, handleListen);
