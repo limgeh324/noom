8f0434c8b83d7d73aab702c8c6919550879a1fd6
limgeh324 <limgeh324@gmail.com>
2022-09-09 01:56:58 +0900

    Chatting Using WebSocket

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 import http from "http";
 import WebSocket from "ws";
 import express from "express";
+import path from "path";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
-app.use("public", express.static(__dirname + "/public"));
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -20,5 +21,28 @@ const handleListen = () => {
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
+  console.log("Connected to Client ✅");
+  socket.on("close", () => {
+    console.log("Disconnected to Client ❌");
+  });
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg.toString());
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) => {
+          aSocket.send(`${socket.nickname}: ${message.payload}`);
+        });
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
+  });
+});
 
 server.listen(3000, handleListen);
